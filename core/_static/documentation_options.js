dcd4434b9521e5cd3f7d39fe821cfee117127844
sbc100 <sbc100@users.noreply.github.com>
2024-08-13 15:40:15 +0000

    deploy: 1bf8e0f5197a169f500762ad9efea97b841a8c7c

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0 (Draft 2024-08-02)',
+    VERSION: '3.0 (Draft 2024-08-13)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
