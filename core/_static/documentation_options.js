a8aa33b5ae1b8964263e7cfe25976569d842965c
backes <backes@users.noreply.github.com>
2024-10-29 16:33:22 +0000

    deploy: 1795669003672403e91392c9bf409c876df4dae5

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0 (Draft 2024-10-21)',
+    VERSION: '3.0 (Draft 2024-10-29)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
